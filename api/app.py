753922998c51398f87757d2c05002d8d7ceaa4b1
aixiu <4815563@qq.com>
2022-10-25 16:10:10 +0800

    init

--- api/app.py
+++ api/app.py
@@ -5,18 +5,15 @@
 
 from http.server import BaseHTTPRequestHandler
 
-def getdata():    
-    returndata = {
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        returndata = {
         "total": 111,
         "contributions": 222
     }
-    return returndata
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        data = getdata()
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(data)
+        self.wfile.write(returndata)
         return
