9c6398232d49ef64f237f6b38f331f38225b1057
aixiu <4815563@qq.com>
2022-10-25 16:19:20 +0800

    init

---
+++ api/index.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Aixiu
+# @Time  : 2022/10/25 11:46:50
+
+from http.server import BaseHTTPRequestHandler
+
+def getdata():    
+    returndata = {
+        "total": 111,
+        "contributions": 222
+    }
+    return returndata
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        data = getdata()
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(data)
+        return
