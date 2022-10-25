93ec4a59a4cd30b83b2c14938698173613592425
aixiu <4815563@qq.com>
2022-10-25 17:33:20 +0800

    init

--- api/index.py
+++ api/index.py
@@ -3,25 +3,21 @@
 # @Author: Aixiu
 # @Time  : 2022/10/25 11:46:50
 
-from http.server import BaseHTTPRequestHandler
-import json
+import uvicorn
+from fastapi import FastAPI, Response
+from api.crawler import main as new
 
-def getdata(name):    
-    returndata = {
-        "total": name,
-        "contributions": name
-    }
-    return returndata
+app = FastAPI()
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        name = self.path
-        data = getdata(name)
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(json.dumps(data).encode('utf-8'))
-        return
-    
-    
+@app.get("/api")
+def news(response: Response, index: int = 0, origin: str = 'zhihu', cache: str = 'null'):
+    response.headers["Cache-Control"] = "max-age=86400, immutable, stale-while-revalidate"
+    response.headers["Content-Type"] = "application/json; charset=utf-8"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    if origin == "undefined":
+        origin = "zhihu"
+    return new(index, origin)
+
+
+if __name__ == "__main__":
+    uvicorn.run("index:app", host="127.0.0.1", port=62, log_level="info")
