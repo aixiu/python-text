5c7c39c0736f4dc377dfafed4fa8158bf78e40e6
aixiu <4815563@qq.com>
2022-10-25 15:48:56 +0800

    init

--- api/index.py
+++ api/index.py
@@ -3,21 +3,11 @@
 # @Author: Aixiu
 # @Time  : 2022/10/25 11:46:50
 
-from http.server import BaseHTTPRequestHandler
+from flask import Flask, request
+import requests
 
+app = Flask(__name__)
 
-def getdata(name):
-    data = {'名字': name}
-    return data
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        path = self.path
-        user = path.split('?')[1]
-        data = getdata(user)
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(data)
-        return
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask Github!'
