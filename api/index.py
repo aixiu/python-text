35e104a8b09ccbabc3533981b5e147dc265348fa
aixiu <4815563@qq.com>
2022-10-25 15:37:19 +0800

    init

--- api/index.py
+++ api/index.py
@@ -4,20 +4,14 @@
 # @Time  : 2022/10/25 11:46:50
 
 from http.server import BaseHTTPRequestHandler
-
-
-def getdata(name):
-    data = {'名字': name}
-    return data
+from cowpy import cow
 
 class handler(BaseHTTPRequestHandler):
+
     def do_GET(self):
-        path = self.path
-        user = path.split('?')[1]
-        data = getdata(user)
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(data)
+        message = cow.Cowacter().milk('Hello from Python from a Serverless Function!')
+        self.wfile.write(message.encode())
         return
